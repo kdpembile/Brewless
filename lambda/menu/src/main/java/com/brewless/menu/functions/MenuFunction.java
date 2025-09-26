6f45231c5cc0f8918228dd1d728919e1ad85eacc
kente <kent.david.embile@gmail.com>
2025-09-26 22:50:10 +0800

    Created menu function

--- lambda/menu/src/main/java/com/brewless/menu/functions/MenuFunction.java
+++ lambda/menu/src/main/java/com/brewless/menu/functions/MenuFunction.java
@@ -1,100 +1,100 @@
 package com.brewless.menu.functions;
 
+import com.brewless.menu.dto.bs.errors.ErrorDto;
 import com.brewless.menu.dto.bs.requests.ApiRequestDto;
 import com.brewless.menu.dto.bs.requests.MenuRequestDto;
 import com.brewless.menu.dto.bs.response.ApiResponseDto;
 import com.brewless.menu.dto.bs.response.PaginationDto;
 import com.brewless.menu.exceptions.InvalidRequestException;
+import com.brewless.menu.exceptions.MenuException;
 import com.brewless.menu.models.bs.Menu;
 import com.brewless.menu.services.MenuService;
+import com.brewless.menu.services.ValidationService;
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Function;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 import reactor.util.function.Tuple2;
-import reactor.util.function.Tuples;
 
 @Slf4j
-@RequiredArgsConstructor
 @Component
 public class MenuFunction {
 
-  public static final String INVALID_REQUEST = "Invalid request";
+  private final String applicationName;
 
   private final MenuService menuService;
 
+  private final ValidationService validationService;
+
+  public MenuFunction(
+      @org.springframework.beans.factory.annotation.Value("${spring.application.name}") String applicationName,
+      MenuService menuService, ValidationService validationService
+  ) {
+    this.applicationName = applicationName;
+    this.menuService = menuService;
+    this.validationService = validationService;
+  }
+
   @Bean
   public Function<ApiRequestDto<MenuRequestDto>, Mono<ApiResponseDto<List<Menu>>>> getMenu() {
-    return request -> Mono.just(validateHeaders(request))
-        .then(validateQueryParams(request))
+    return request -> validationService.validateHeaders(request)
+        .then(validationService.validateQueryParams(request))
         .flatMap(tuple2 -> menuService.getMenu(tuple2.getT1(), tuple2.getT2())
-            .switchIfEmpty(Mono.defer(() -> {
-              PaginationDto pagination = PaginationDto.builder()
-                  .page(tuple2.getT1())
-                  .size(tuple2.getT2())
-                  .build();
-
-              String message = "No menu items found for page %s with size %s"
-                  .formatted(tuple2.getT1(), tuple2.getT2());
-
-              ApiResponseDto<List<Menu>> response = new ApiResponseDto<>();
-              response.setData(Collections.emptyList());
-              response.setMessage(message);
-              response.setTimeStamp(LocalDateTime.now());
-              response.setPaginationDto(pagination);
-
-              return Mono.just(response);
-            })));
+            .switchIfEmpty(Mono.defer(() -> buildFallBackResponse(tuple2))))
+        .onErrorResume(this::mapError);
   }
 
-  private Mono<Tuple2<Integer, Integer>> validateQueryParams(
-      ApiRequestDto<MenuRequestDto> menuRequestApiRequestDto) {
-    int page = 0;
-    int size = 100;
+  private Mono<ApiResponseDto<List<Menu>>> buildFallBackResponse(Tuple2<Integer, Integer> tuple2) {
+    PaginationDto pagination = PaginationDto.builder()
+        .page(tuple2.getT1())
+        .size(tuple2.getT2())
+        .build();
 
-    Map<String, String> queryParams = menuRequestApiRequestDto.getQueryParams();
+    String message =
+        "No menu items found for page %s with size %s"
+        .formatted(tuple2.getT1(), tuple2.getT2());
 
-    if (queryParams != null) {
-      if (queryParams.containsKey("page")) {
-        try {
-          page = Integer.parseInt(queryParams.get("page"));
-        } catch (NumberFormatException e) {
-          return Mono.error(new InvalidRequestException(INVALID_REQUEST, e));
-        }
-      }
+    ApiResponseDto<List<Menu>> response = new ApiResponseDto<>();
+    response.setData(Collections.emptyList());
+    response.setMessage(message);
+    response.setTimeStamp(LocalDateTime.now());
+    response.setPaginationDto(pagination);
 
-      if (queryParams.containsKey("size")) {
-        try {
-          size = Integer.parseInt(queryParams.get("size"));
-        } catch (NumberFormatException e) {
-          return Mono.error(new InvalidRequestException(INVALID_REQUEST, e));
-        }
-      }
-    }
-    return Mono.just(Tuples.of(page, size));
+    return Mono.just(response);
   }
 
+  private Mono<ApiResponseDto<List<Menu>>> mapError(Throwable throwable) {
+    if (throwable instanceof MenuException menuException) {
+      log.error("[{}] Menu fetch failed", applicationName, menuException);
 
-  private Mono<Void> validateHeaders(
-      ApiRequestDto<MenuRequestDto> menuRequestApiRequestDto) {
+      ApiResponseDto<List<Menu>> apiResponseDto = new ApiResponseDto<>();
+      apiResponseDto.setErrorDto(ErrorDto.builder()
+          .code(menuException.getErrorCode())
+          .message(menuException.getMessage())
+          .build());
 
-    Map<String, String> headers = menuRequestApiRequestDto.getHeaders();
-    String correlationId = headers.get("x-correlation-id");
-    String channel = headers.get("channel-name");
+      return Mono.just(apiResponseDto);
+    } else if (throwable instanceof InvalidRequestException invalidRequestException) {
+      log.error("[{}] Menu fetch failed", applicationName, invalidRequestException);
 
-    if (correlationId == null
-        || correlationId.isEmpty() || channel == null || channel.isEmpty()) {
-      log.error(INVALID_REQUEST);
+      ApiResponseDto<List<Menu>> apiResponseDto = new ApiResponseDto<>();
+      apiResponseDto.setErrorDto(ErrorDto.builder()
+          .code(invalidRequestException.getErrorCode())
+          .message(invalidRequestException.getMessage())
+          .build());
 
-      return Mono.error(new InvalidRequestException(
-          INVALID_REQUEST, new RuntimeException("Something went wrong")));
     }
-    return Mono.empty();
+
+    ApiResponseDto<List<Menu>> apiResponseDto = new ApiResponseDto<>();
+    apiResponseDto.setErrorDto(ErrorDto.builder()
+        .code("M00002")
+        .message(throwable.getMessage())
+        .build());
+
+    return Mono.just(apiResponseDto);
   }
 }

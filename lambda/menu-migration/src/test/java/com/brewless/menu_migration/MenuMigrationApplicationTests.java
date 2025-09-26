7555488a22b785c537c306dfb14e5907883c1cc9
kente <kent.david.embile@gmail.com>
2025-09-26 12:06:14 +0800

    Created menu migration function

--- lambda/menu-migration/src/test/java/com/brewless/menu_migration/MenuMigrationApplicationTests.java
+++ lambda/menu-migration/src/test/java/com/brewless/menu_migration/MenuMigrationApplicationTests.java
@@ -3,7 +3,7 @@ package com.brewless.menu_migration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.liquibase.enabled=false")
 class MenuMigrationApplicationTests {
 
 	@Test

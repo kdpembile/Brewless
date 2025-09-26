f6a2517fc18e124c66ae3724d6f34fb53bd65ca9
kente <kent.david.embile@gmail.com>
2025-09-26 10:03:54 +0800

    Created menu migration function

--- lambda/menu-migration/src/main/java/com/brewless/menu_migration/functions/MenuMigrationFunction.java
+++ lambda/menu-migration/src/main/java/com/brewless/menu_migration/functions/MenuMigrationFunction.java
@@ -1,16 +1,21 @@
 package com.brewless.menu_migration.functions;
 
-import java.util.function.Supplier;
+import java.util.function.Function;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class MenuMigrationFunction {
 
   @Bean
-  public Supplier<String> syncMenu() {
-    return () -> "Successfully sync menu database";
+  public Function<String, String> syncMenu() {
+    return payload -> {
+      log.info("Request payload: {}", payload);
+      return "Successfully sync menu database";
+    };
   }
 }

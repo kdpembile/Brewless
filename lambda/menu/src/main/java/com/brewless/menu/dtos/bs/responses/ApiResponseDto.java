7122e090912596d71965ca719843111afa72cdab
kente <kent.david.embile@gmail.com>
2025-10-04 12:54:24 +0800

    Created menu function

--- lambda/menu/src/main/java/com/brewless/menu/dtos/bs/responses/ApiResponseDto.java
+++ lambda/menu/src/main/java/com/brewless/menu/dtos/bs/responses/ApiResponseDto.java
@@ -1,10 +1,12 @@
 package com.brewless.menu.dtos.bs.responses;
 
 import com.brewless.menu.dtos.bs.errors.ErrorDto;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import java.time.LocalDateTime;
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponseDto<T> {
 
   private T data;
